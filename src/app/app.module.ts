f4163ada2853be9d738dd9124b53fd6e414620b7
Luis Sardon <lmsardon@gmail.com>
2017-09-22 15:54:28 -0500

    scss globals implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { GlobalHeaderComponent } from './global-header/global-header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GlobalHeaderComponent
   ],
   imports: [
     BrowserModule,
